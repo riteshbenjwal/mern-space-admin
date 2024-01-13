035a835c3ee211d3d4f753710704f26e8e4d8b7d
riteshgeeky <ritesh.benjwal@geeky.dev>
2024-01-13 16:23:15 +0530

    login api add

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,21 +1,31 @@
-import { Layout, Card, Space, Form, Input, Checkbox, Button, Flex } from "antd";
-import { LockFilled, UserOutlined, LockOutlined } from "@ant-design/icons";
-import Logo from "../../components/icons/Logo";
+import { Layout, Card, Space, Form, Input, Checkbox, Button, Flex, Alert } from 'antd';
+import { LockFilled, UserOutlined, LockOutlined } from '@ant-design/icons';
+import Logo from '../../components/icons/Logo';
+import { useMutation } from '@tanstack/react-query';
+import { Credentials } from '../../types';
+import { login } from '../../http/api';
+
+const loginUser = async (credentials: Credentials) => {
+    // server call logic
+    const { data } = await login(credentials);
+    return data;
+};
 
 const LoginPage = () => {
+    const { mutate, isPending, isError, error } = useMutation({
+        mutationKey: ['login'],
+        mutationFn: loginUser,
+        onSuccess: async () => {
+            console.log('Login successful.');
+        },
+    });
+
     return (
         <>
-            <Layout
-                style={{ height: "100vh", display: "grid", placeItems: "center" }}
-            >
+            <Layout style={{ height: '100vh', display: 'grid', placeItems: 'center' }}>
                 <Space direction="vertical" align="center" size="large">
                     <Layout.Content
-                        style={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    >
+                        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <Logo />
                     </Layout.Content>
                     <Card
@@ -23,35 +33,38 @@ const LoginPage = () => {
                         style={{ width: 300 }}
                         title={
                             <Space
-                                style={{
-                                    width: "100%",
-                                    fontSize: 16,
-                                    justifyContent: "center",
-                                }}
-                            >
+                                style={{ width: '100%', fontSize: 16, justifyContent: 'center' }}>
                                 <LockFilled />
                                 Sign in
                             </Space>
-                        }
-                    >
+                        }>
                         <Form
                             initialValues={{
                                 remember: true,
                             }}
-                        >
+                            onFinish={(values) => {
+                                mutate({ email: values.username, password: values.password });
+                                console.log(values);
+                            }}>
+                            {isError && (
+                                <Alert
+                                    style={{ marginBottom: 24 }}
+                                    type="error"
+                                    message={error?.message}
+                                />
+                            )}
                             <Form.Item
                                 name="username"
                                 rules={[
                                     {
                                         required: true,
-                                        message: "Please input your Username",
+                                        message: 'Please input your Username',
                                     },
                                     {
-                                        type: "email",
-                                        message: "Email is not valid",
+                                        type: 'email',
+                                        message: 'Email is not valid',
                                     },
-                                ]}
-                            >
+                                ]}>
                                 <Input prefix={<UserOutlined />} placeholder="Username" />
                             </Form.Item>
                             <Form.Item
@@ -59,14 +72,10 @@ const LoginPage = () => {
                                 rules={[
                                     {
                                         required: true,
-                                        message: "Please input your password",
+                                        message: 'Please input your password',
                                     },
-                                ]}
-                            >
-                                <Input.Password
-                                    prefix={<LockOutlined />}
-                                    placeholder="Password"
-                                />
+                                ]}>
+                                <Input.Password prefix={<LockOutlined />} placeholder="Password" />
                             </Form.Item>
                             <Flex justify="space-between">
                                 <Form.Item name="remember" valuePropName="checked">
@@ -80,8 +89,8 @@ const LoginPage = () => {
                                 <Button
                                     type="primary"
                                     htmlType="submit"
-                                    style={{ width: "100%" }}
-                                >
+                                    style={{ width: '100%' }}
+                                    loading={isPending}>
                                     Log in
                                 </Button>
                             </Form.Item>
