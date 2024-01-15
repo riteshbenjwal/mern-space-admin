2070fbd6c36ed7a7e9a34e95a2960d4db1f096f4
riteshbenjwal <riteshbenjwal7@gmail.com>
2024-01-15 22:04:47 +0530

    maintaining auth state on page refresh

--- src/router.tsx
+++ src/router.tsx
@@ -2,26 +2,33 @@ import { createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Dashboard from './layouts/Dashboard';
 import NonAuth from './layouts/NonAuth';
+import Root from './layouts/Root';
 import LoginPage from './pages/Login/login';
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <Dashboard />,
+        element: <Root />,
         children: [
             {
                 path: '',
-                element: <HomePage />,
+                element: <Dashboard />,
+                children: [
+                    {
+                        path: '',
+                        element: <HomePage />,
+                    },
+                ],
             },
-        ],
-    },
-    {
-        path: '/auth',
-        element: <NonAuth />,
-        children: [
             {
-                path: 'login',
-                element: <LoginPage />,
+                path: '/auth',
+                element: <NonAuth />,
+                children: [
+                    {
+                        path: 'login',
+                        element: <LoginPage />,
+                    },
+                ],
             },
         ],
     },
