ec4b6f1f8bb023b1282581f5279748c99d817def
riteshbenjwal <riteshbenjwal7@gmail.com>
2024-01-21 14:22:31 +0530

    user filter design and add filter interactivity

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,21 +1,22 @@
-import { Breadcrumb, Space, Table } from "antd";
-import { RightOutlined } from "@ant-design/icons";
-import { Link, Navigate } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import { getUsers } from "../../http/api";
-import { User } from "../../types";
-import { useAuthStore } from "../../store";
+import { Breadcrumb, Space, Table } from 'antd';
+import { RightOutlined } from '@ant-design/icons';
+import { Link, Navigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getUsers } from '../../http/api';
+import { User } from '../../types';
+import { useAuthStore } from '../../store';
+import UsersFilter from './UsersFilter';
 
 const columns = [
     {
-        title: "ID",
-        dataIndex: "id",
-        key: "id",
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
     },
     {
-        title: "Name",
-        dataIndex: "firstName",
-        key: "firstName",
+        title: 'Name',
+        dataIndex: 'firstName',
+        key: 'firstName',
         render: (_text: string, record: User) => {
             return (
                 <div>
@@ -25,14 +26,14 @@ const columns = [
         },
     },
     {
-        title: "Email",
-        dataIndex: "email",
-        key: "email",
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
     },
     {
-        title: "Role",
-        dataIndex: "role",
-        key: "role",
+        title: 'Role',
+        dataIndex: 'role',
+        key: 'role',
     },
 ];
 
@@ -43,7 +44,7 @@ const Users = () => {
         isError,
         error,
     } = useQuery({
-        queryKey: ["users"],
+        queryKey: ['users'],
         queryFn: () => {
             return getUsers().then((res) => res.data);
         },
@@ -51,21 +52,27 @@ const Users = () => {
 
     const { user } = useAuthStore();
 
-    if (user?.role !== "admin") {
+    if (user?.role !== 'admin') {
         return <Navigate to="/" replace={true} />;
     }
 
     return (
         <>
-            <Space direction="vertical" size="large" style={{ width: "100%" }}>
+            <Space direction="vertical" size="large" style={{ width: '100%' }}>
                 <Breadcrumb
                     separator={<RightOutlined />}
-                    items={[{ title: <Link to="/">Dashboard</Link> }, { title: "Users" }]}
+                    items={[{ title: <Link to="/">Dashboard</Link> }, { title: 'Users' }]}
                 />
                 {isLoading && <div>Loading...</div>}
                 {isError && <div>{error.message}</div>}
 
-                <Table columns={columns} dataSource={users} />
+                <UsersFilter
+                    onFilterChange={(filterName: string, filterValue: string) => {
+                        console.log(filterName, filterValue);
+                    }}
+                />
+
+                <Table columns={columns} dataSource={users} rowKey={'id'} />
             </Space>
         </>
     );
